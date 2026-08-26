8461c9747623160b94be523a8c70099fcc80d447
vgv <vgv@silentwings.ru>
2026-08-26 21:39:47 +0500

    prinf

--- TcpServ/main.c
+++ TcpServ/main.c
@@ -3,15 +3,19 @@
 #include "set_addres_func.c"
 #include "tcp_functions.c"
 #include "error_func.c"
+#include "read_n_func.c"
 
 SERVER_FUNC_FORWARD_DECL
+
+#define BYTE uchar
 
 int main(int argc, char** argv) {
 
 	INIT();
 
-	SOCKET masterSocket;
-	SOCKET slaveSocket; // listening
+	SOCKET listenSocket;
+	SOCKET connectSocket;
+
 	char* host = NULL;
 	char* port = DEFAULT_PORT; // 9000
 
@@ -25,22 +29,29 @@ int main(int argc, char** argv) {
 	printf("host defined as %s\n", host);
 	printf("port defined as %s\n", port);
 
-	masterSocket = tcp_server(host, port, &local);
+	listenSocket = tcp_server(host, port, &local);
 
 	int peerLen = 0;
 
 	do {
-		peerLen = sizeof(from);
-		slaveSocket = accept(masterSocket, SA &from, (socklen_t*) &peerLen);
-		printf("connection accepted");
-
-		if (!IS_VALID_SOCKET( slaveSocket )) {
+		socklen_t peerLen = sizeof(from);
+		connectSocket = accept(listenSocket, SA &from, &peerLen);
+		if (!IS_VALID_SOCKET( connectSocket )) {
 			error( 1, errno, "SOCKET FUNC MISTAKE");
 		}
 
+		printf("connection accepted\n");
+
+		char buffer[512];
+		int bytesRecieved = readvrec(connectSocket, buffer, sizeof(buffer));
+
+		if (bytesRecieved == 0 ) {
+			CLOSE(connectSocket);
+			printf("Client is Offline \n");
+			break;
+		}
+
 	} while (TRUE);
-
-
 
 	EXIT(0);
 }
